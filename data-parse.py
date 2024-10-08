79647ad6f92179af4817ab76f6e129f89e2716cc
sammygee8213 <gebhardts5070@my.uwstout.edu>
2024-10-08 03:27:24 +0000

    Python data parsing file

---
+++ data-parse.py
@@ -0,0 +1,36 @@
+import xml.etree.ElementTree as ET
+import re
+import json
+import yaml
+xml = ET.parse("myfile.xml")
+root = xml.getroot()
+
+ns = re.match('{.*}', root.tag).group(0)
+editconf = root.find("{}edit-config".format(ns))
+defop = editconf.find("{}default-operation".format(ns))
+testop = editconf.find("{}test-option".format(ns))
+
+print("The default-operation contains: {}".format(defop.text))
+print("The test-option contains: {}".format(testop.text))
+
+with open('myfile.json', 'r') as json_file:
+    ourjson = json.load(json_file)
+
+print(ourjson)
+
+print("The access token is: {}".format(ourjson['access_token']))
+print("The token expires in {} seconds.".format(ourjson['expires_in']))
+
+print("\n\n---")
+print(yaml.dump(ourjson))
+
+with open('myfile.yaml','r') as yaml_file:
+    ouryaml = yaml.safe_load(yaml_file)
+
+print(ouryaml)
+
+print("The access token is {}".format(ouryaml['access_token']))
+print("The token expires in {} seconds.".format(ouryaml['expires_in']))
+
+print("\n\n")
+print(json.dumps(ouryaml, indent=4))
